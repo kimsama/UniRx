6a2c3fef2fd04fffbf73f432592ae0f4de0d6169
kimsama <hwpeterkim@gmail.com>
2015-04-14 11:06:05 +0900

    ngui event test sample with RX.

---
+++ Assets/UniRx/Examples/NGUI/Sample.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace UniRx.Examples
+{
+    public class Sample : MonoBehaviour
+    {
+
+        public UIButton button;
+
+        void Start()
+        {
+
+            button.onClick.AsObservable().Subscribe(_ => { });
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+    }
+}
