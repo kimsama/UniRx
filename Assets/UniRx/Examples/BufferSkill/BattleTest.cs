328bb929a23480a564463c2b0bc47fb0ef408c84
kimsama <hwpeterkim@gmail.com>
2015-04-14 16:08:46 +0900

    first commit with a simple Subject/Observable sample.

---
+++ Assets/UniRx/Examples/BufferSkill/BattleTest.cs
@@ -0,0 +1,193 @@
+﻿using UnityEngine;
+using System;
+using System.Collections;
+
+using FluentInterfaceExample.Types;
+using FluentInterfaceExample.Types.ExpressionBuilder;
+using FluentInterfaceExample;
+
+using UniRx;
+
+
+public class BattleTest : MonoBehaviour 
+{
+
+    System.Random ran = new System.Random((int)DateTime.Now.Ticks);
+    CharacterBuilder builder = new CharacterBuilder();
+
+    Character hero, enemy;
+
+    Subject<Character> heroDeadSubject, enemyDeadSubject;
+
+	// Use this for initialization
+	void Start () 
+    {
+        // Build our Character with the expression builder.
+        builder.Create("Valient")
+            .As(Character.ClassType.Fighter)
+            .WithAge(22)
+            .HP(20)
+            .Str(18)
+            .Agi(14)
+            .Int(12);
+
+        // Get our Character.
+        hero = builder.Value();
+        enemy = null;
+
+        heroDeadSubject = new Subject<Character>();
+        heroDeadSubject.AsObservable().Where(c => c.IsAlive == false).Subscribe(c => Debug.Log("Dead HP: " + c.HP.ToString()));
+        
+        //This will not subscribe due to the given expression of Where does not match the condition.
+        //heroDeadSubject.OnNext(hero);
+
+        StartCoroutine(DoUpdate());
+	}
+	
+	// Update is called once per frame
+	IEnumerator DoUpdate () 
+    {
+        // Put our hero to battle against endless enemies, and see how long he survives!
+        while (hero.IsAlive)
+        {
+            if (enemy == null || !enemy.IsAlive)
+            {
+                // Build an enemy with the expression builder.
+                builder.Create(CommonHelper.GenerateRandomName())
+                    .As((Character.ClassType)ran.Next(4))
+                    .WithAge(ran.Next(12, 200))
+                    .HP(ran.Next(5, 12))
+                    .Str(ran.Next(21))
+                    .Agi(ran.Next(21))
+                    .Int(ran.Next(21))
+                    .Gold(ran.Next(50));
+
+                // Get our enemy.
+                enemy = builder.Value();
+            }
+            else 
+            {
+                // Display start of battle.
+                CommonHelper.DisplayStartOfBattle(hero, enemy);
+
+                // Battle time!
+                //CommonHelper.Battle(hero, enemy);
+                yield return StartCoroutine(Battle(hero, enemy));
+                //yield return Battle(hero, enemy);
+            }
+
+            //;
+            //yield return StartCoroutine(WaitForKey());
+            if (hero == null)
+                yield break;
+        }
+
+        Debug.LogError("+++ End of Game +++");
+
+        yield return 0;
+	}
+
+    /// <summary>
+    /// Simple battle system.
+    /// </summary>
+    /// <param name="hero">Character</param>
+    /// <param name="enemy">Character</param>
+    public IEnumerator Battle(Character hero, Character enemy)
+    {
+        // Battle the enemy.
+        while (hero.IsAlive && enemy.IsAlive)
+        {
+            // Print quick stats.
+            Debug.Log(hero.QuickStats() + " / " + enemy.QuickStats());
+
+            yield return new WaitForSeconds(1);
+
+            // Hero attacks!
+            Debug.Log(Attack(hero, enemy));
+
+            yield return new WaitForSeconds(1);
+
+            // Enemy attacks!
+            Debug.Log(Attack(enemy, hero));
+
+            // Prompt for next round of combat.
+            if (hero.IsAlive)
+            {
+                Debug.Log(">");
+            }
+
+            //This will subscribe due to the given expression of observable's Where does match the condition.
+            heroDeadSubject.OnNext(hero);
+
+            yield return 0;
+        }
+
+        // Analyze battle results.
+        if (hero.IsAlive)
+        {
+            // Hero won!
+            hero.Gold += enemy.Gold;
+
+            Debug.Log("Our hero survives to fight another battle! Won " + enemy.Gold + " gold!");
+            Debug.Log(">");
+            enemy = null;
+        }
+        else
+        {
+            // Enemy won!
+            Debug.Log("Our hero has fallen with " + hero.Gold + " gold! The world is covered in darkness once again.");
+        }
+
+        yield return 0;
+    }
+
+    /// <summary>
+    /// Verbs used for attacking.
+    /// </summary>
+    private static readonly string[] _attackVerbs =
+        {
+            "slashes",
+            "stabs",
+            "smashes",
+            "impales",
+            "poisons",
+            "shoots",
+            "incinerates",
+            "destroys"
+        };
+
+    /// <summary>
+    /// Attacks a Character and returns status message.
+    /// </summary>
+    /// <param name="attacker">Character initiating attack</param>
+    /// <param name="defender">Character to attack</param>
+    /// <returns>Status message</returns>
+    private string Attack(Character attacker, Character defender)
+    {
+        string result = "";
+
+        System.Random ran = new System.Random((int)DateTime.Now.Ticks);
+
+        // Calculate damage.
+        int damage = ran.Next(10);
+
+        // Deduct damage from defender's HP.
+        defender.HP -= damage;
+
+        // Select an attack verb.
+        string verb = _attackVerbs[ran.Next(_attackVerbs.Length)];
+
+        // Create status message.
+        result = attacker.Name + " " + verb + " " + defender.Name + " for " + damage + " damage!";
+
+        return result;
+    }
+
+    IEnumerator WaitForKey()
+    {
+        while(!Input.anyKeyDown)
+            yield return 0;
+
+        yield return 0;
+    }
+}
---
+++ Assets/UniRx/Examples/BufferSkill/CommonHelper.cs
@@ -0,0 +1,86 @@
+﻿using UnityEngine;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using FluentInterfaceExample.Types;
+
+namespace FluentInterfaceExample
+{
+    /// <summary>
+    /// Helper class to store our static strings and helper methods.
+    /// </summary>
+    public static class CommonHelper
+    {
+        #region String Helpers
+
+        /// <summary>
+        /// Words used for enemy first names.
+        /// </summary>
+        private static readonly string[] _firstNames =
+        {
+            "Destro",
+            "Victo",
+            "Mozri",
+            "Fang",
+            "Ovi",
+            "Hell",
+            "Syth",
+            "End"
+        };
+
+        /// <summary>
+        /// Words used for enemy last names.
+        /// </summary>
+        private static readonly string[] _lastNames =
+        {
+            "math",
+            "rin",
+            "sith",
+            "icous",
+            "ravage",
+            "wrath",
+            "ryn",
+            "less"
+        };
+
+
+
+        /// <summary>
+        /// Generates a random name from the static string lists.
+        /// </summary>
+        /// <returns>Random name</returns>
+        public static string GenerateRandomName()
+        {
+            string result = "";
+
+            System.Random ran = new System.Random((int)DateTime.Now.Ticks);
+            result = _firstNames[ran.Next(_firstNames.Length)] + _lastNames[ran.Next(_lastNames.Length)];
+
+            return result;
+        }
+
+        #endregion
+
+        /// <summary>
+        /// Simple suspense text.
+        /// </summary>
+        /// <param name="hero">Character</param>
+        /// <param name="enemy">Character</param>
+        public static void DisplayStartOfBattle(Character hero, Character enemy)
+        {
+            // Size up the battle statistics.
+            Debug.LogWarning("=== Starting Battle ===");
+            Debug.Log(hero.ToString() + "vs " + enemy.ToString());
+
+            // Add suspense.
+            Debug.Log("An enemy approaches> ");
+            //Console.ReadKey();
+            //Debug.Log();
+            //Debug.Log();
+        }
+
+
+    }
+}
