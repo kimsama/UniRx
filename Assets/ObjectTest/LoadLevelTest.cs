613753127e1254876e2b9b2320cf30605d588557
kimsama <hwpeterkim@gmail.com>
2016-03-29 16:20:07 +0900

    Modified to use UnityEngine.SceneManager with Unity 5.3.x instead of using Application.LoadLevel.

--- Assets/ObjectTest/LoadLevelTest.cs
+++ Assets/ObjectTest/LoadLevelTest.cs
@@ -1,9 +1,7 @@
+#if !UNITY_5_3
 using System;
 using System.Collections;
 using UnityEngine;
-#if !(UNITY_4 || UNITY_5_0 || UNITY_5_1 || UNITY_5_2)
-using UnityEngine.SceneManagement;
-#endif
 
 namespace UniRx.ObjectTest
 {
@@ -11,28 +9,22 @@ namespace UniRx.ObjectTest
     {
         void Start()
         {
-            var ll = GameObject.Find("LoadLevel");
-            var llcolor = ll.GetComponent<GUIText>().color;
-            var cll = ll.AddComponent<Clicker>();
-#if !(UNITY_4 || UNITY_5_0 || UNITY_5_1 || UNITY_5_2)
-            cll.OnClicked += () => SceneManager.LoadScene("LoadLevelTestNew");
-#else
-            cll.OnClicked += () => Application.LoadLevel("LoadLevelTestNew");
-#endif
-            cll.OnEntered += () => cll.GetComponent<GUIText>().color = Color.blue;
-            cll.OnExited += () => cll.GetComponent<GUIText>().color = llcolor;
+            //var ll = GameObject.Find("LoadLevel");
+            //var llcolor = ll.GetComponent<GUIText>().color;
+            //var cll = ll.AddComponent<Clicker>();
+            //cll.OnClicked += () => Application.LoadLevel("LoadLevelTestNew");
+            //cll.OnEntered += () => cll.GetComponent<GUIText>().color = Color.blue;
+            //cll.OnExited += () => cll.GetComponent<GUIText>().color = llcolor;
 
-            var lla = GameObject.Find("LoadLevelAdditive");
-            var llacolor = lla.GetComponent<GUIText>().color;
-            var clla = lla.AddComponent<Clicker>();
+            //var lla = GameObject.Find("LoadLevelAdditive");
+            //var llacolor = lla.GetComponent<GUIText>().color;
+            //var clla = lla.AddComponent<Clicker>();
 
-#if !(UNITY_4 || UNITY_5_0 || UNITY_5_1 || UNITY_5_2)
-            clla.OnClicked += () => SceneManager.LoadScene("LoadLevelTestAdditive", LoadSceneMode.Additive);
-#else
-            clla.OnClicked += () => Application.LoadLevelAdditive("LoadLevelTestAdditive");
-#endif
-            clla.OnEntered += () => clla.GetComponent<GUIText>().color = Color.blue;
-            clla.OnExited += () => clla.GetComponent<GUIText>().color = llacolor;
+            //clla.OnClicked += () => Application.LoadLevelAdditive("LoadLevelTestAdditive");
+            //clla.OnEntered += () => clla.GetComponent<GUIText>().color = Color.blue;
+            //clla.OnExited += () => clla.GetComponent<GUIText>().color = llacolor;
         }
     }
 }
+
+#endif
